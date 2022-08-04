39d9ba3e5459ead238dc2ae8c4b39b985688e018
Samsа <ira05teplyakova@gmail.com>
2022-08-04 16:57:25 +0300

    test.terem

---
+++ main.js
@@ -0,0 +1,28 @@
+$(function(){
+  jQuery.fn.swap = function(b) {
+    b = jQuery(b)[0];
+    var a = this[0],
+        a2 = a.cloneNode(true),
+        b2 = b.cloneNode(true),
+        stack = this;
+
+    a.parentNode.replaceChild(b2, a);
+    b.parentNode.replaceChild(a2, b);
+
+    stack[0] = a2;
+    return this.pushStack( stack );
+};
+
+$('button').on('click', function(){
+  $('.left').swap('.right');
+});
+  });
+
+
+
+  function chpok(id){
+    elem = document.getElementById(id); //находим блок div по его id, который передали в функцию
+    state = elem.style.display; //смотрим, включен ли сейчас элемент
+    if (state =='') elem.style.display='none'; //если включен, то выключаем
+    else elem.style.display=''; //иначе - включаем
+}
